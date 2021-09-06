0a692659970e6dc2caae47263c7c8875e7fc1087
Petra Dueck <pechadee@gmail.com>
2021-09-06 16:34:15 -0700

    Initial Commit

---
+++ SourceControlXcode12/SourceControlXcode12App.swift
@@ -0,0 +1,17 @@
+//
+//  SourceControlXcode12App.swift
+//  SourceControlXcode12
+//
+//  Created by Petra Dueck on 2021-09-06.
+//
+
+import SwiftUI
+
+@main
+struct SourceControlXcode12App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
